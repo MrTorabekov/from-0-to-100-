61c398d8f3e8497d149569195263c0ee449d129e
MrTorabekov <diyorbektorabekov08@gmail.com>
2024-10-06 19:25:42 +0500

    by Torabekov_08

--- higher-order-functions/homework.py
+++ higher-order-functions/homework.py
@@ -105,18 +105,18 @@ from datetime import datetime
 
 # Adding Syntactic Sugar -> Sintactic shakar qo'shish   #noqa
 
-def decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-
-    return wrapper
-
-
-@decorator
-def say_whee():
-    print("Whee!")
-
-say_whee = decorator(say_whee)
-say_whee()
+# def decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#
+#     return wrapper
+#
+#
+# @decorator
+# def say_whee():
+#     print("Whee!")
+#
+# say_whee = decorator(say_whee)
+# say_whee()
