e6bb4f6d813f94e479df64b33c317dc5d0f0e56e
MrTorabekov <diyorbektorabekov08@gmail.com>
2024-10-22 15:38:25 +0500

    by Torabekov_08

---
+++ working with json/lesson.py
@@ -0,0 +1,29 @@
+import json
+
+# users = {
+#     "name":"Diyorbek",
+#     "age": 16,
+#     "city":"Tashkent",
+#     "language": ["UZB","ENG"],
+#     "jobs":"Programming",
+#     "programming":["Python","go","rust"],
+#     "level": "Junior +"
+# }
+#
+# json_obj = json.dumps(users,indent=4)
+#
+# with open('AboutMe.json','w') as file:
+#     file.write(json_obj)
+
+
+b = [
+    {"name": "lola", "age": "22", "grade": "A"},
+    {"name": "Olim", "age": "22", "grade": "B"},
+    {"name": "Ali", "age": "22", "grade": "C"},
+]
+# json_obj = json.dumps(b,indent=4)
+with open("users.json",'r') as file:
+    data = json.load(file)
+    print(data[0]["name"])
+    print(data[1]["name"])
+
